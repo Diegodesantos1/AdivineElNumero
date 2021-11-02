b00e43a5bfa78152497bd652268c81e486e3a3fb
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2021-11-02 12:00:06 +0100

    Contador de intentos

--- adivinenumero.py
+++ adivinenumero.py
@@ -4,12 +4,16 @@ int(numero)
 
 print("Dime un número")
 num=int(input())
+n_intentos=1
 while num != numero:
     if numero < num:
         print("Te has pasado, selecciona otro número")
         num=int(input())
+        n_intentos += 1
     if numero > num:
         print("Te has quedado corto, selecciona otro número")
         num=int(input())
+        n_intentos += 1
 if numero == num:
     print("¡Has acertado, enhorabuena!")
+    print(f"Has necesitado {n_intentos} intentos")
