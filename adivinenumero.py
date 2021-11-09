bf77ea92908a42fe40b588e6affc7605a9ce0c66
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2021-11-09 10:54:52 +0100

    Limitación de intentos en el nivel fácil

--- adivinenumero.py
+++ adivinenumero.py
@@ -6,12 +6,13 @@ print("Selecciona el nivel de dificultad 1:Simple, 2:Intermedio, 3:Avanzado, 4:E
 nivel=int(input())
 
 if nivel ==1:
-    print("Has seleccionado el nivel simple")
+    print("Has seleccionado el nivel simple, tienes que adivinar un número del 0 al 99 y sólo tienes 20 intentos")
     int(numero)
     print("Dime un número")
     num=int(input())
     n_intentos=1
-    while num != numero:
+    while num != numero and n_intentos < 20:
+        print(f"Llevas ya {n_intentos} intentos, cuidado")
         if numero < num:
             print("Te has pasado, selecciona otro número menor")
             num=int(input())
