967bdabf94a6c88d7d408e7a047ee569a231e2ec
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2021-11-09 11:13:46 +0100

    Creación últimos niveles de dificultad y arreglado el código

--- adivinenumero.py
+++ adivinenumero.py
@@ -55,12 +55,37 @@ if nivel ==2:
 if nivel ==3:
     import random
     numero=random.randint (0,1000000)
-    print("Has seleccionado el nivel Intermedio, tienes que adivinar un número del 0 al 1.000.000 y sólo tienes 80 intentos")
+    print("Has seleccionado el nivel Avanzado, tienes que adivinar un número del 0 al 1.000.000 y sólo tienes 80 intentos")
     int(numero)
     print("Dime un número")
     num=int(input())
     n_intentos=1
     n_intentos_max=80
+    while num != numero and n_intentos < n_intentos_max:
+        print(f"Llevas ya {n_intentos} intentos, cuidado, te quedan {n_intentos_max-n_intentos}")
+        if numero < num:
+            print("Te has pasado, selecciona otro número menor")
+            num=int(input())
+            n_intentos += 1
+        if numero > num:
+            print("Te has quedado corto, selecciona otro número mayor")
+            num=int(input())
+            n_intentos += 1
+    if n_intentos >= n_intentos_max:
+        print("Se te han acabado los intentos, derrota")
+    if numero == num:
+        print("¡Has acertado, enhorabuena!")
+        print(f"Has necesitado {n_intentos} intentos")
+
+if nivel ==4:
+    import random
+    numero=random.randint (0,1000000000000)
+    print("Has seleccionado el nivel Experto, tienes que adivinar un número del 0 al 1.000.000.000.000 y sólo tienes 160 intentos")
+    int(numero)
+    print("Dime un número")
+    num=int(input())
+    n_intentos=1
+    n_intentos_max=160
     while num != numero and n_intentos < n_intentos_max:
         print(f"Llevas ya {n_intentos} intentos, cuidado, te quedan {n_intentos_max-n_intentos}")
         if numero < num:
